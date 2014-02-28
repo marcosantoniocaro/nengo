ec7e89210311d3f283c26e4aceadd2c38f35c2c2
Terry Stewart <tcstewar@uwaterloo.ca>
2014-02-28 00:24:16 -0500

    Updated spa to use latest syntax
    I had some rebase weirdnesses in the original spa branch, so I started
    this one fresh.

---
+++ nengo/tests/test_spa.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pytest
+
+import nengo
+import nengo.spa as spa
+
+def test_spa_basic():
+
+    class SpaTestBasic(spa.SPA):
+        class Rules:
+            def a():
+                match(state='A')
+                effect(state='B')
+            def b():
+                match(state='B')
+                effect(state='C')
+            def c():
+                match(state='C')
+                effect(state='D')
+            def d():
+                match(state='D')
+                effect(state='E')
+            def e():
+                match(state='E')
+                effect(state='A')
+        
+        def make(self):
+            self.state = spa.Buffer('state', dimensions=32)
+            
+            self.bg = spa.BasalGanglia('bg', rules=self.Rules)
+            self.thal = spa.Thalamus('thal', 'bg')
+            
+            def state_input(t):
+                if t<0.1: return 'A'
+                else: return '0'
+            self.input = spa.Input('state',state_input) 
+
+    model = nengo.Model()
+    with model:
+        s = SpaTestBasic(label='spa')
+            
+        pState = nengo.Probe(s.state.state.output, 'output', filter=0.03)
+        pRules = nengo.Probe(s.thal.rules.output, 'output', filter=0.03)
+    
+    sim = nengo.Simulator(model)
+    sim.run(1)
+    
+    vectors = s.get_module_output('state')[1].vectors.T
+    import pylab
+    pylab.subplot(2, 1, 1)
+    pylab.plot(np.dot(sim.data(pState), vectors))
+    pylab.subplot(2, 1, 2)
+    pylab.plot(sim.data(pRules))
+    pylab.show()
+    
+    
+    
+if __name__ == "__main__":
+    nengo.log(debug=True)
+    pytest.main([__file__, '-v'])
