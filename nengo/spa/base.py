540a175e255dfd8905c145aad2068c754f48e021
Terry Stewart <tcstewar@uwaterloo.ca>
2014-02-28 00:24:16 -0500

    Updated spa to use latest syntax
    I had some rebase weirdnesses in the original spa branch, so I started
    this one fresh.

---
+++ nengo/spa/base.py
@@ -0,0 +1,53 @@
+from . import vocab
+from ..objects import Network
+
+class Module(Network):
+    def __init__(self, name, *args, **kwargs):
+        self.inputs = {}
+        self.outputs = {}
+        self.name = name
+        kwargs['label'] = name
+        Network.__init__(self, *args, **kwargs)
+
+    def on_add(self, spa):
+        for k, (obj, v) in self.inputs.iteritems():
+            if type(v)==int:
+                self.inputs[k] = (obj, spa.get_default_vocab(v))
+        for k, (obj, v) in self.outputs.iteritems():
+            if type(v)==int:
+                self.outputs[k] = (obj, spa.get_default_vocab(v))
+        
+
+class SPA(Network):
+    def __init__(self, *args, **kwargs):
+        self.modules = {}
+        self.default_vocabs = {}
+        Network.__init__(self, *args, **kwargs)
+        
+    def get_default_vocab(self, dimensions):
+        if dimensions not in self.default_vocabs:
+            self.default_vocabs[dimensions] = vocab.Vocabulary(dimensions)
+        return self.default_vocabs[dimensions]    
+        
+    def add(self, obj):
+        Network.add(self, obj)
+        
+        if isinstance(obj, Module):
+            assert obj.name not in self.modules
+            self.modules[obj.name]=obj
+            obj.on_add(self)
+            
+    def get_module_input(self, name):
+        if name in self.modules:
+            return self.modules[name].inputs['default']
+        elif '_' in name:
+            module, name = name.split('_', 1)
+            return self.modules[module].inputs[name]
+            
+    def get_module_output(self, name):
+        if name in self.modules:
+            return self.modules[name].outputs['default']
+        elif '_' in name:
+            module, name = name.split('_', 1)
+            return self.modules[module].outputs[name]
+
