036f29cdf23760f555de99bc29a9c1a9bec4b65e
Terry Stewart <tcstewar@uwaterloo.ca>
2014-04-09 23:15:22 -0400

    Added docs to spa.Cortical

--- nengo/spa/cortical.py
+++ nengo/spa/cortical.py
@@ -44,20 +44,45 @@ class Cortical(Module):
 
     @property
     def bias(self):
+        """Return a bias node; create it if needed."""
         if self._bias is None:
             with self:
                 self._bias = nengo.Node([1])
         return self._bias
 
-    def add_direct_effect(self, target_name, text):
+    def add_direct_effect(self, target_name, value):
+        """Make a fixed constant input to a module.
+
+        Parameters
+        ----------
+        target_name : string
+            The name of the module input to use
+        value : string
+            A semantic pointer to be sent to the module input
+        """
         sink, vocab = self.spa.get_module_input(target_name)
-        transform = np.array([vocab.parse(text).v]).T
+        transform = np.array([vocab.parse(value).v]).T
 
         with self.spa:
             nengo.Connection(self.bias, sink, transform=transform,
                              filter=self.filter)
 
     def add_route_effect(self, target_name, source_name, transform):
+        """Connect a module outtput to a module input
+
+        Parameters
+        ----------
+        target_name : string
+            The name of the module input to affect
+        source_name : string
+            The name of the module output to read from.  If this output uses
+            a different Vocabulary than the target, a linear transform
+            will be applied to convert from one to the other.
+        transform : string
+            A semantic point to convolve with the source value before
+            sending it into the target.  This transform takes
+            place in the source Vocabulary.
+        """
         target, target_vocab = self.spa.get_module_input(target_name)
         source, source_vocab = self.spa.get_module_output(source_name)
 
