2fd615cee81a5aefc261d4a77e071a5b570541bf
Terry Stewart <tcstewar@uwaterloo.ca>
2014-04-09 23:07:50 -0400

    Documentation and testing for spa.Thalamus

---
+++ nengo/spa/tests/test_thalamus.py
@@ -0,0 +1,70 @@
+import pytest
+import nengo
+import nengo.spa as spa
+import numpy as np
+
+
+def test_thalamus():
+    class SPA(spa.SPA):
+        def __init__(self):
+            spa.SPA.__init__(self, rng=np.random.RandomState(2))
+            self.vision = spa.Buffer(dimensions=16)
+            self.motor = spa.Buffer(dimensions=16)
+            self.motor2 = spa.Buffer(dimensions=32)
+
+            actions = spa.Actions(
+                'dot(vision, A) --> motor=A',
+                'dot(vision, B) --> motor=vision, motor2=vision*A*~B',
+                )
+            self.bg = spa.BasalGanglia(actions)
+            self.thalamus = spa.Thalamus(self.bg)
+
+            def input(t):
+                if t < 0.1:
+                    return 'A'
+                elif t < 0.3:
+                    return 'B'
+                else:
+                    return '0'
+            self.input = spa.Input(vision=input)
+
+    model = SPA()
+
+    with model:
+        input, vocab = model.get_module_input('motor')
+        input2, vocab2 = model.get_module_input('motor2')
+        p = nengo.Probe(input, 'output', filter=0.03)
+        p2 = nengo.Probe(input2, 'output', filter=0.03)
+
+    model.seed = 3
+    sim = nengo.Simulator(model)
+    sim.run(0.3)
+
+    data = vocab.dot(sim.data[p].T)
+    data2 = vocab2.dot(sim.data[p2].T)
+
+    assert 0.9 < data[0, 100] < 1.1
+    assert -0.2 < data[0, 299] < 0.2
+    assert 0.9 < data[1, 299] < 1.1
+    assert -0.2 < data[1, 100] < 0.2
+    assert 0.6 < data2[0, 299] < 0.8
+    assert -0.2 < data2[0, 100] < 0.2
+
+
+def test_errors():
+    class SPA(spa.SPA):
+        def __init__(self):
+            spa.SPA.__init__(self, rng=np.random.RandomState(2))
+            self.vision = spa.Buffer(dimensions=16)
+
+            actions = spa.Actions(
+                '0.5 --> motor=A'
+                )
+            self.bg = spa.BasalGanglia(actions)
+    with pytest.raises(NameError):
+        SPA()
+
+
+if __name__ == '__main__':
+    nengo.log(debug=True)
+    pytest.main([__file__, '-v'])
--- nengo/spa/thalamus.py
+++ nengo/spa/thalamus.py
@@ -7,60 +7,96 @@ import numpy as np
 
 
 class Thalamus(Module):
-    def __init__(self, bg, neurons_per_rule=50, inhibit=1, pstc_inhibit=0.008,
-                 output_filter=0.01, rule_threshold=0.2,
-                 neurons_per_channel_dim=50, channel_subdim=16,
-                 channel_pstc=0.01, neurons_cconv=200,
-                 neurons_gate=40, gate_threshold=0.3,
-                 pstc_to_gate=0.002):
+    """A thalamus, implementing the effects for an associated BasalGanglia
+
+    Parameters
+    ----------
+    bg : spa.BasalGanglia
+        The associated basal ganglia that defines the action to implement
+    neurons_action : int
+        Number of neurons per action to represent selection
+    inhibit : float
+        Strength of inhibition between actions
+    filter_inhibit : float
+        Synaptic filter to apply for inhibition between actions
+    filter_bg : float
+        Synaptic filter for connection between basal ganglia and thalamus
+    filter_direct : float
+        Synaptic filter for direct outputs
+    threshold_action : float
+        Minimum value for action representation
+    neurons_channel_dim : int
+        Number of neurons per routing channel dimension
+    subdim_channel : int
+        Number of subdimensions used in routing channel
+    filter_channel : float
+        Synaptic filter for channel inputs and outputs
+    neurons_cconv : int
+        Number of neurons per circular convolution dimension
+    neurons_gate : int
+        Number of neurons per gate
+    threshold_gate : float
+        Minimum value for gating neurons
+    filter_to-gate : float
+        Synaptic filter for controlling a gate
+    """
+    def __init__(self, bg, neurons_action=50, threshold_action=0.2,
+                 inhibit=1, filter_inhibit=0.008, filter_bg=0.008,
+                 filter_direct=0.01,
+                 neurons_channel_dim=50, subdim_channel=16,
+                 filter_channel=0.01,
+                 neurons_cconv=200,
+                 neurons_gate=40, threshold_gate=0.3, filter_to_gate=0.002):
         Module.__init__(self)
         self.bg = bg
-        self.neurons_per_rule = neurons_per_rule
+        self.neurons_action = neurons_action
         self.inhibit = inhibit
-        self.pstc_inhibit = pstc_inhibit
-        self.output_filter = output_filter
-        self.rule_threshold = rule_threshold
-        self.neurons_per_channel_dim = neurons_per_channel_dim
-        self.channel_subdim = channel_subdim
-        self.channel_pstc = channel_pstc
+        self.filter_inhibit = filter_inhibit
+        self.filter_direct = filter_direct
+        self.threshold_action = threshold_action
+        self.neurons_channel_dim = neurons_channel_dim
+        self.subdim_channel = subdim_channel
+        self.filter_channel = filter_channel
         self.neurons_gate = neurons_gate
         self.neurons_cconv = neurons_cconv
-        self.gate_threshold = gate_threshold
-        self.pstc_to_gate = pstc_to_gate
+        self.threshold_gate = threshold_gate
+        self.filter_to_gate = filter_to_gate
+        self.filter_bg = filter_bg
 
-        self.gates = {}
-        self.channels = {}
+        self.gates = {}    # gating ensembles per action (created as needed)
+        self.channels = {} # channels to pass transformed data between modules
 
     def on_add(self, spa):
         Module.on_add(self, spa)
         self.spa = spa
 
-        N = self.bg.actions.count
+        N = self.bg.actions.count  # number of actions
 
         with self:
-            actions = nengo.networks.EnsembleArray(
-                nengo.LIF(self.neurons_per_rule),
-                N, dimensions=1,
-                intercepts=nengo.objects.Uniform(self.rule_threshold, 1),
+            # An EnsembleArray to store which action is selected
+            self.actions = nengo.networks.EnsembleArray(
+                nengo.LIF(self.neurons_action),
+                N, dimensions=1, encoders = [[1.0]] * self.neurons_action,
+                intercepts=nengo.objects.Uniform(self.threshold_action, 1),
                 label='actions')
-            self.actions = actions
 
-            for ens in actions.ensembles:
-                ens.encoders = [[1.0]] * self.neurons_per_rule
+            # bias input to keep the value up in the absence of basal
+            # basal ganglia inhibition
+            self.bias = nengo.Node(output=[1], label='bias')
+            nengo.Connection(self.bias, self.actions.input,
+                             transform=np.ones((N, 1)), filter=None)
 
-            bias = nengo.Node(output=[1], label='bias')
-            self.bias = bias
-
-            nengo.Connection(actions.output, actions.input,
+            # mutual inhibition on the actions
+            nengo.Connection(self.actions.output, self.actions.input,
                              transform=(np.eye(N)-1)*self.inhibit,
-                             filter=self.pstc_inhibit)
-
-            nengo.Connection(bias, actions.input, transform=np.ones((N, 1)),
-                             filter=None)
+                             filter=self.filter_inhibit)
 
         with spa:
-            nengo.Connection(self.bg.output, actions.input, filter=None)
+            # connect basal ganglia to thalamus
+            nengo.Connection(self.bg.output, self.actions.input,
+                             filter=self.filter_bg)
 
+        # implement the various effects
         for i, action in enumerate(self.bg.actions.actions):
             for name, effects in action.effect.effect.items():
                 for effect in effects.items:
@@ -72,66 +108,104 @@ class Thalamus(Module):
                     else:
                         raise NotImplementedError('Cannot handle %s' % effect)
 
-    def add_direct_effect(self, index, target_name, text):
+    def add_direct_effect(self, index, target_name, value):
+        """Cause an action to drive a particular module input to value.
+
+        Parameters
+        ----------
+        index : int
+            The action number that causes this effect
+        target_name : string
+            The name of the module input to connect to
+        value : string
+            A semantic pointer to be sent into the module when this action
+            is active
+        """
         sink, vocab = self.spa.get_module_input(target_name)
-        transform = np.array([vocab.parse(text).v]).T
+        transform = np.array([vocab.parse(value).v]).T
 
         with self.spa:
             nengo.Connection(self.actions.ensembles[index],
                              sink, transform=transform,
-                             filter=self.output_filter)
+                             filter=self.filter_direct)
 
     def get_gate(self, index):
+        """Return the gate for an action
+
+        The gate will be created if it does not already exist.  The gate
+        neurons have no activity when the action is selected, but are
+        active when the action is not selected.  This makes the gate useful
+        for inhibiting ensembles that should only be active when this
+        action is active.
+        """
         if index not in self.gates:
             with self:
-                intercepts = nengo.objects.Uniform(self.gate_threshold, 1)
+                intercepts = nengo.objects.Uniform(self.threshold_gate, 1)
                 gate = nengo.Ensemble(nengo.LIF(self.neurons_gate),
                                       dimensions=1,
                                       intercepts=intercepts,
                                       label='gate[%d]' % index,
                                       encoders=[[1]] * self.neurons_gate)
                 nengo.Connection(self.actions.ensembles[index], gate,
-                                 filter=self.pstc_to_gate, transform=-1)
+                                 filter=self.filter_to_gate, transform=-1)
                 nengo.Connection(self.bias, gate, filter=None)
                 self.gates[index] = gate
-
         return self.gates[index]
 
     def add_route_effect(self, index, target_name, source_name, transform):
+        """Set an action to send source to target with the given transform
+
+        Parameters
+        ----------
+        index : int
+            The action number that will cause this effect
+        target_name : string
+            The name of the module input to affect
+        source_name : string
+            The name of the module output to read from.  If this output uses
+            a different Vocabulary than the target, a linear transform
+            will be applied to convert from one to the other.
+        transform : string
+            A semantic point to convolve with the source value before
+            sending it into the target.  This transform takes
+            place in the source Vocabulary.
+        """
         with self:
             gate = self.get_gate(index)
 
             target, target_vocab = self.spa.get_module_input(target_name)
             source, source_vocab = self.spa.get_module_output(source_name)
 
+            # build a communication channel between the source and target
             dim = target_vocab.dimensions
-            subdim = self.channel_subdim
+            subdim = self.subdim_channel
             assert dim % subdim == 0  # TODO: check this somewhere
 
             channel = nengo.networks.EnsembleArray(
-                nengo.LIF(self.neurons_per_channel_dim*subdim),
+                nengo.LIF(self.neurons_channel_dim*subdim),
                 dim/subdim, dimensions=subdim,
                 label='channel_%d_%s' % (index, target_name))
 
-            nengo.Connection(channel.output, target, filter=self.channel_pstc)
-
-            inhibit = [[-1]]*(self.neurons_per_channel_dim*subdim)
+            # inhibit the channel when the action is not chosen
+            inhibit = [[-1]]*(self.neurons_channel_dim*subdim)
             for e in channel.ensembles:
                 nengo.Connection(gate, e.neurons, transform=inhibit,
-                                 filter=self.pstc_inhibit)
+                                 filter=self.filter_inhibit)
 
+        with self.spa:
+            # compute the requested transform
             t = source_vocab.parse(transform).get_convolution_matrix()
+            # handle conversion between different Vocabularies
             if target_vocab is not source_vocab:
                 t = np.dot(source_vocab.transform_to(target_vocab), t)
 
-        with self.spa:
+            # connect source to target
             nengo.Connection(source, channel.input, transform=t,
-                             filter=self.channel_pstc)
+                             filter=self.filter_channel)
+            nengo.Connection(channel.output, target, filter=self.filter_channel)
+
 
 """
-
-
-
 if hasattr(source, 'convolve'):
     # TODO: this is an insanely bizarre computation to have to do
     #   whenever you want to use a CircConv network.  The parameter
